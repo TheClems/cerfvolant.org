eedf2dfab1ec370536b70f1bbe738898412d09df
Clément TS <clement@thillay.net>
2024-01-26 19:40:49 +0100

    Template of the page

--- chat.php
+++ chat.php
@@ -1,5 +1,33 @@
+<html lang="frfr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="chat.css">
+    <title>Chat</title>
+</head>
+<body>
+    <nav>
+        <a>A</a>
+        <a>B</a>
+        <a>C</a>
+        <a>D</a>
+    </nav>
+    <div class="split">
+        <div class="contacts-bar">
+        </div>
+        <div class="chat">
+            <div class="chat-history">
+            </div>
+            <div class="chat-input">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
 <?php
 
+require 'baseDeDonnee.php';
 
 
 ?>
