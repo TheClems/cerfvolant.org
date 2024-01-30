405aacb9be366658be6ee92a377e26dd8d2e1533
Clément TS <clement@thillay.net>
2024-01-30 17:53:34 +0100

    Create the script for insert account data in db

--- signIn.php
+++ signIn.php
@@ -4,22 +4,25 @@
     <title>Register</title>
 </head>
 <body>
-<form method="post">
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required><br>
+<form action="#" method="post">
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required><br>
 
-        <label for="userName">Nom d'utilisateur :</label>
-        <input type="text" id="userName" name="userName" required><br>
+    <label for="userName">Nom d'utilisateur :</label>
+    <input type="text" id="userName" name="userName" required><br>
 
-        <label for="passWord">Mot de passe :</label>
-        <input type="passWord" id="passWord" name="passWord" required><br>
+    <label for="passWord">Mot de passe :</label>
+    <input type="passWord" id="passWord" name="passWord" required><br>
 
-        <input type="submit" value="S'inscrire">
-    </form>
+    <input type="submit" value="S'inscrire">
+</form>
 </body>
 </html>
 
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 require 'baseDeDonnee.php';
 
 $email = $_POST['email'];
@@ -27,4 +30,13 @@ $username = $_POST['userName'];
 $password = $_POST['passWord'];
 
 session_start();
+
+$sql = "INSERT INTO User(email, userName, passWord) VALUES ('$email', '$username', '$password')";
+
+if (mysqli_query($conn, $sql)) {
+} else {
+      echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
 ?>
