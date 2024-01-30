46dbfc576246b731608805152d7804c70e5a1674
Clément TS <clement@thillay.net>
2024-01-30 21:30:32 +0100

    PHP script update

--- register.php
+++ register.php
@@ -2,21 +2,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Join cerfvolant.org</title>
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="register.css">
 </head>
 <body>
-<form action="" method="post">
-    <label for="email">Email :</label>
-    <input type="email" id="email" name="email" value="" required><br>
-
-    <label for="userName">Nom d'utilisateur :</label>
-    <input type="text" id="userName" name="userName" required><br>
-
-    <label for="passWord">Mot de passe :</label>
-    <input type="passWord" id="passWord" name="passWord" required><br>
-
-    <input type="submit" value="S'inscrire">
-</form>
+    <img src="logo.png" class="logo" alt="logo">
+    <h1>Join cerfvolant.org</h1>
+    <form action="" method="post">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" value="" required><br>
+    
+        <label for="userName">UserName</label>
+        <input type="text" id="userName" name="userName" required><br>
+    
+        <label for="passWord">Password</label>
+        <input type="passWord" id="passWord" name="passWord" required><br>
+    
+        <input type="submit" value="S'inscrire" class="submit">
+    </form>
 </body>
 </html>
 
@@ -26,10 +30,7 @@ ini_set("display_errors", 1);
 session_start();
 
 require 'baseDeDonnee.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $username = $_POST['userName'];
     $password = $_POST['passWord'];
@@ -45,14 +46,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $delete = "DELETE FROM User WHERE email = ''";
     
     if ($result=mysqli_query($conn, $sql2)) {
-        
         $rows = mysqli_num_rows($result);
-    
-    
-        if ($rows)
-        {
-            while($row=mysqli_fetch_array($result))
-            {
+
+        if ($rows) {
+            while($row=mysqli_fetch_array($result)) {
                 if (mysqli_query($conn, $delete)) {
                 }
                 else {
@@ -61,17 +58,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             }
         }
     } 
-    else 
-    {
+    else {
         echo "Erreur : " . $sql2 . "<br>" . mysqli_error($conn);
     }
-    
- 
+
+    sleep(2);
+
+    $sql10 = "SELECT * FROM User WHERE email = '".$email."'";
+
+    if ($result=mysqli_query($conn, $sql10)) {
+        $rows = mysqli_num_rows($result);
+
+        if ($rows)
+        {
+            while($row=mysqli_fetch_array($result)) {
+                $user = $row;
+            }
+        }
+    } 
+    else {
+        echo "Erreur : " . $sql10 . "<br>" . mysqli_error($conn);
+    }
+
+    if ($user) {
+
+        if ($password=$user['passWord']) {
+            $_SESSION['utilisateur'] = $user;
+            exit();
+        } 
+        else {
+            echo "<p style='text-align:center;'>" ."Mot de passe incorrect". "</p>";
+        }
+    }
+    else {
+        echo  "<p style='text-align:center;'>"."Adresse email ou mot de passe invalide". "</p>";
+    }
+
+    header('Location: register.php');
 }
 
-    
-if (isset($_SESSION['utilisateur'])) 
-{
+if (isset($_SESSION['utilisateur'])) {
     $utilisateur = $_SESSION['utilisateur'];
     $id = $utilisateur['id'];
     $userName = $utilisateur['userName'];
