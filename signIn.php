d7e936cc6bfbad7ffaefd29337fdd14729818a0f
Clément TS <clement@thillay.net>
2024-01-30 17:30:31 +0100

    Create form

--- signIn.php
+++ signIn.php
@@ -1,5 +1,24 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+<form method="post">
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required><br>
+
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+</html>
+
 <?php
-
-
 
 ?>
