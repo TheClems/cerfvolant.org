cd12ca3a3314cc0eb1ceac8aeb4f68e6c394c7d5
Clément TS <clement@thillay.net>
2024-02-14 11:33:38 +0100

    Add scipt in script-chat.js

--- script-chat.js
+++ script-chat.js
@@ -1,1 +1,22 @@
 // Script js for the chat.php page
+setInterval('load_messages()', 500);
+setInterval('load_friends()', 500);
+
+function load_messages(){
+    $('#messages-list').load('loadmessage.php?myID=<?php echo $MYID_URL; ?>&recipientID=<?php echo $RECIPIENTID_URL; ?>');
+    
+}
+function load_friends(){
+    $('#contacts-bar').load('loadfriends.php');
+    
+}
+
+var chatContainer = document.getElementById("#messages-list");
+
+    function scrollToBottom() {
+  const el = document.getElementById("messages-list");
+    el.scrollTop = el.scrollHeight;
+}
+
+
+scrollToBottom()
