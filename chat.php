91e7e4a21625fdd21c3a6463f377fcf5c7a389a7
Clément TS <clement@thillay.net>
2024-02-15 07:35:46 +0100

    Link between chat.php and script js

--- chat.php
+++ chat.php
@@ -64,6 +64,7 @@ if($RECIPIENTID_URL==="null"){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="chat.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="script-chat.js"></script>
 
     <title>Chat</title>
 </head>
